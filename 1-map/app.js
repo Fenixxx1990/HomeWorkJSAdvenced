1d1f244d9b0bf98806deb754e0b063e39f2f4945
Fenix <fenixxx1990@yandex.ru>
2026-02-09 00:34:41 +0400

    homework corrections

--- 1-map/app.js
+++ 1-map/app.js
@@ -4,8 +4,14 @@ const array = [
   { id: 1, name: "Вася" },
   { id: 2, name: "Петя" },
   { id: 1, name: "Вася" },
+  { id: 2, name: "Петя" },
 ];
 
 const newSet = new Set(array.map((item) => item.name));
 
-console.log(newSet);
+const clearArray = [...newSet].map((name) => ({
+  id: array.find((item) => item.name === name).id,
+  name: name,
+}));
+
+console.log(clearArray);
