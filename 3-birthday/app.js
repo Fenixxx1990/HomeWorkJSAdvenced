f33ae4bb5ebbe13917ffa98ed417f00eb3fd67bb
Fenix <fenixxx1990@yandex.ru>
2026-02-09 20:57:36 +0400

    finish 3 homework

--- 3-birthday/app.js
+++ 3-birthday/app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function isAgeValid(birthdayString) {
+  const today = new Date();
+  const birthdayDate = new Date(birthdayString);
+  const different = today - birthdayDate;
+  return Math.floor(different / (1000 * 60 * 60 * 24 * 365)) > 14;
+}
+
+console.log(isAgeValid("2006-01-01"));
