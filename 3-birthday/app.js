74d873398a6110189d69405397abf4e1415f1048
Fenix <fenixxx1990@yandex.ru>
2026-02-09 20:20:10 +0400

    first commit

--- 3-birthday/app.js
+++ 3-birthday/app.js
@@ -1,8 +1,1 @@
 "use strict";
-
-function rollOfDice(diceModel) {
-  const maxValue = Number(diceModel.replace("d", ""));
-  return Math.floor(Math.random() * maxValue + 1);
-}
-
-console.log(rollOfDice("d20"));
