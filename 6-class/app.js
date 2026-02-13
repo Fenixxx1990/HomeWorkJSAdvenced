8e4f1f1e382d18a41a2a6f9bcf43cf266d11e967
Fenix <fenixxx1990@yandex.ru>
2026-02-13 10:56:08 +0400

    6-class

---
+++ 6-class/app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const Pers = function (race, name, language) {
+  this.race = race;
+  this.name = name;
+  this.language = language;
+};
+
+Pers.prototype.speak = function () {
+  console.log(this.language, this.name);
+};
+
+const OrcClass = function (race, name, language) {
+  Pers.call(this, race, name, language);
+};
+
+OrcClass.prototype = Object.create(Pers.prototype);
+
+OrcClass.prototype.kick = function () {
+  console.log("I kick");
+};
+
+const ELfClass = function (race, name, language) {
+  Pers.call(this, race, name, language);
+};
+
+ELfClass.prototype = Object.create(Pers.prototype);
+
+ELfClass.prototype.createMagick = function () {
+  console.log("I create magick");
+};
+
+const orc = new OrcClass("Orc", "Grom", "Orcish");
+orc.kick();
+orc.speak();
+const elf = new ELfClass("Elf", "Legolas", "Elvish");
+elf.createMagick();
+elf.speak();
